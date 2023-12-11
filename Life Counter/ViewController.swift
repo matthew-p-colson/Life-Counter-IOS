aee7f7340d881487ad57fde747a641c2aa8bff8b
matthew colson <matthew.p.colson@gmail.com>
2023-12-10 22:02:32 -0600

    Updated layout

--- Life Counter/ViewController.swift
+++ Life Counter/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lifeValueLabel: UILabel!
+    
+    var lifeValue = 20;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func upButtonPressed(_ sender: UIButton) {
+        lifeValue += 1
+        updateUI()
+    }
+    
+    @IBAction func downButtonPressed(_ sender: UIButton) {
+        if lifeValue > 0 {
+            lifeValue -= 1
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        lifeValueLabel.text = String(lifeValue)
+    }
 }
 
